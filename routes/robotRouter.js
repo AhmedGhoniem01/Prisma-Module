5714faa7f0a8248403e0890eaa4061c43c4fe67d
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-07-03 18:34:49 +0200

    modified robot router

--- routes/robotRouter.js
+++ routes/robotRouter.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const robotController = require('../controller/robotController')
 
-router.get('/get/user/:userID', robotController.getUserRobots)
 router.get('/get/id/:robotID', robotController.getRobotByID)
 router.get('/get/address/:robotAdress', robotController.getRobotByAddress)
-router.get('/get/', robotController.getAllRobots)
+router.get('/get/user/:userID', robotController.getUserRobots)
 router.get('/get/connected/user/:userID', robotController.getUserConnectedRobots)
 router.get('/get/connected', robotController.getAllConnectedRobots)
+router.get('/get/', robotController.getAllRobots)
 
 module.exports = router
